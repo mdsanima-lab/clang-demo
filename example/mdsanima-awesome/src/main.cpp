adab7ea47add056eb0311b98a367e868f05c5d98
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-24 22:43:41 +0100

    style: align codebase with 2-space indentation standard
    Standardized indentation across the codebase to 2 spaces for better
    consistency and alignment with common style practices. This change
    affects `.clang-format` and `.editorconfig` settings as well as **C**
    and **C++** example projects.
    
    Additionally, refined project description comments and streamlined
    example code output for clarity.

--- example/mdsanima-awesome/src/main.cpp
+++ example/mdsanima-awesome/src/main.cpp
@@ -1,8 +1,10 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
+// Example C++ implementation of the MDSANIMA LAB project.
+
 #include <iostream>
 
 int main() {
-    std::cout << "This is example of MDSANIMA AWESOME project!" << std::endl;
+  std::cout << "This is the MDSANIMA AWESOME project." << std::endl;
 }
--- example/mdsanima-cprogram/src/main.c
+++ example/mdsanima-cprogram/src/main.c
@@ -1,10 +1,12 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
+// Example C implementation of the MDSANIMA LAB project.
+
 #include <stdio.h>
 
 int main(void) {
-    printf("This is example code of MDSANIMA C Program!\n");
+  printf("This is the MDSANIMA C Program!\n");
 
-    return 0;
+  return 0;
 }
