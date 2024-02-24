b46fa78798aec521036412477306c1902720de26
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-24 17:29:35 +0100

    feat: add example code for base c project (#7)

---
+++ example/mdsanima-cprogram/src/main.c
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+#include <stdio.h>
+
+int main(void) {
+    printf("This is example code of MDSANIMA C Program!\n");
+
+    return 0;
+}
