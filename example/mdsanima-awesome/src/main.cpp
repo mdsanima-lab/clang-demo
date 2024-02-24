6435a18c88c03b4bf04965a41f417006f13f2328
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-24 17:11:51 +0100

    feat: add example code for base cpp project
    Initialized a new **C++** project `mdsanima-awesome` with a basic
    structure and README instructions. Included a simple `main.cpp` source
    file that outputs a project description when executed.
    
    The README provides clear build and run commands to facilitate quick
    setup for developers.

---
+++ example/mdsanima-awesome/src/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+#include <iostream>
+
+int main() {
+    std::cout << "This is example of MDSANIMA AWESOME project!" << std::endl;
+}
