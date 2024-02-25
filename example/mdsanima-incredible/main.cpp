b87cc04d5aa21b69cc5774e163ff90a1a5451282
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-25 22:21:07 +0100

    style: refine commentary across multiple projects (#18)

--- example/mdsanima-incredible/main.cpp
+++ example/mdsanima-incredible/main.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// Example C++ implementation of the example MDSANIMA INCREDIBLE project.
+// Example C++ implementation of the MDSANIMA INCREDIBLE project.
 
 #include <iostream>
 
