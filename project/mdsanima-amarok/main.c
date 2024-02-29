4aeb3f826946787311e28a5737c7e05ae14103ba
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-29 18:59:56 +0100

    feat: add mdsanima-amarok project with install targets
    Introduced _mdsanima-amarok_ as a new example in the project, along
    with corresponding executable and static library build configurations.
    
    The CMake setup has been updated to include installation targets for
    the executable, library, and header files. This enhancement facilitates
    easier distribution and reuse of the _mdsanima-amarok_ module, providing
    a color print functionality for text in terminal applications.

---
+++ project/mdsanima-amarok/main.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+// Simple C implementation of the MDSANIMA AMAROK project.
+
+#include <mdsanima.h>
+#include <stdio.h>
+
+int main(void) {
+  const int redColor = 196;
+
+  cprint("Hello World from the MDSANIMA AMAROK\n", redColor);
+
+  return 0;
+}
