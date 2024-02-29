ddc94f4aae133bf42d0eae0be75de0b188afe412
Marcin Różewski <marcinrozewski@gmail.com>
2024-02-29 19:02:59 +0100

    feat: add mdsanima-amarok project with install targets (#21)

---
+++ project/mdsanima-amarok/mdsanima.c
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+// Simple C implementation of the MDSANIMA AMAROK project.
+
+#include <stdio.h>
+
+void cprint(char* text, int foreground) {
+  printf("\e[38;5;%dm%s\e[0m", foreground, text);
+}
