0c70c5f399b70f000e3c2ecb666ff7b69d19177d
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-23 16:27:56 +0200

    feat: add background parameter on cprint

--- project/mdsanima-amarok/mdsanima.h
+++ project/mdsanima-amarok/mdsanima.h
@@ -1,17 +1,20 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// Simple C implementation of the MDSANIMA AMAROK project.
+// The demo MDSANIMA C library on the demo MDSANIMA AMAROK project.
 
 #ifndef MDSANIMA_H
 #define MDSANIMA_H
 
 /**
- * This is a color print. You can change the foreground color of the text.
+ * This is a simple color printing implementation. You can change the foreground and the background
+ * color of the text that will be printed in the terminal. This function is a wrapper for the ANSI
+ * escape sequence from the terminal.
  *
- * @param text The text to print in the terminal.
+ * @param text The text to print in the terminal with the color.
  * @param foreground The foreground color is a number from 0 to 255.
+ * @param background The background color is a number from 0 to 255.
  */
-void cprint(char* text, int foreground);
+void cprint(char* text, int foreground, int background);
 
 #endif
