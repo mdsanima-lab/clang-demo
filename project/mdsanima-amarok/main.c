fb501b2850ac2b6d9cc71b6bf523b2731f970830
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-23 16:33:01 +0200

    feat: add background parameter on cprint (#32)

--- project/mdsanima-amarok/main.c
+++ project/mdsanima-amarok/main.c
@@ -1,15 +1,19 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// Simple C implementation of the MDSANIMA AMAROK project.
+// Simple C implementation of the demo MDSANIMA AMAROK project. This project is a simple demo of
+// the basic usage of the MDSANIMA C library that can build and run on amd64 GNU/Linux system, such
+// as Debian, Ubuntu, Fedora, CentOS, and so on. The main funcionality of this project is to print
+// text in the terminal with a color.
 
 #include <mdsanima.h>
 #include <stdio.h>
 
 int main(void) {
-    const int redColor = 196;
+    const int foregroundColor = 45;
+    const int backgroundColor = 27;
 
-    cprint("Hello World from the MDSANIMA AMAROK\n", redColor);
+    cprint(" MDSANIMA AMAROK \n", foregroundColor, backgroundColor);
 
     return 0;
 }
--- project/mdsanima-amarok/mdsanima.c
+++ project/mdsanima-amarok/mdsanima.c
@@ -1,8 +1,10 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// Simple C implementation of the MDSANIMA AMAROK project.
+// The demo MDSANIMA C library on the demo MDSANIMA AMAROK project.
 
 #include <stdio.h>
 
-void cprint(char* text, int foreground) { printf("\e[38;5;%dm%s\e[0m", foreground, text); }
+void cprint(char* text, int foreground, int background) {
+    printf("\e[38;5;%dm\e[48;5;%dm%s\e[0m", foreground, background, text);
+}
