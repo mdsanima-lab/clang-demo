8324237471a4d320197e507913e362f6d2d236af
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-24 22:15:42 +0200

    feat: add demo conquest project and cleanup
    Added the `mdsanima-conquest` program, including its build, linking,
    and installation setup in the CMake configuration. This expansion
    introduces another practical demonstration of the `MDSANIMA` library's
    capabilities. Concurrently, the`mdsanima-deeply` example has been
    removed to focus our examples more tightly around the current project
    scope and utility. This refactoring simplifies the project structure
    and makes way for more focused future development.
    
    Refinements include updating the `README.md` to reflect these changes,
    ensuring users have up-to-date instructions for building, running, and
    installing the demo programs. The documentation has been adjusted to
    include `mdsanima-conquest`, providing a comprehensive view of available
    demos.
    
    Additionally, minor updates to the library's core files `mdsanima.c`
    and `mdsanima.h` refine the documentation comments and incorporate
    necessary headers, improving the clarity and maintainability of the
    codebase.
    
    These changes collectively enhance the project's demonstration of the
    `MDSANIMA` library's versatility and ease of integration into user
    projects, streamlining the build process and documentation for better
    developer experience.

--- lib/mdsanima.c
+++ lib/mdsanima.c
@@ -1,8 +1,9 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// The `MDSANIMA` demo C library for the demo `MDSANIMA AMAROK` and `MDSANIMA BLIZZARD` projects.
+// The `MDSANIMA` demo library implementation.
 
+#include <mdsanima.h>
 #include <stdio.h>
 
 void cprint(char* text, int foreground, int background) {
