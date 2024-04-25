99c3fe54ab2133fea55ccfb4bf3eaea3177babcf
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-25 03:06:09 +0200

    refactor: use shared library in incredible example (#41)

---
+++ example/mdsanima-incredible/lib.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+// Simple C++ example demo of `INCREDIBLE` shared library implementation.
+
+#include "lib.hpp"
+
+#include <iostream>
+
+void mprint(const char* text) { std::cout << text << '\n'; }
