c460bc35b5c26d67f0f67e7f86ab446c17aa6ca6
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-25 03:30:15 +0200

    refactor: use shared library in stunning example
    This program is the same as `mdsanima-fantastic` but in different
    file extension for the demo purpose only.
    
    Related: #40

---
+++ example/mdsanima-stunning/lib.cxx
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
+// Licensed under the MIT license.
+
+// Simple C++ example demo of `STUNNING` shared library implementation.
+
+#include "lib.hxx"
+
+#include <iostream>
+
+void mprint(const char* text) { std::cout << text << '\n'; }
