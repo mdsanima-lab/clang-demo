893f32fdde63be1b66827149d333f1483c789537
Marcin Różewski <marcinrozewski@gmail.com>
2024-04-25 07:57:37 +0200

    feat: add color text print function across projects (#44)

--- example/mdsanima-incredible/lib.hpp
+++ example/mdsanima-incredible/lib.hpp
@@ -1,14 +1,30 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// Simple C++ example demo of `MDSANIMA INCREDIBLE` shared library header file.
+// The header C++ example demo project file for `MDSANIMA INCREDIBLE` shared library.
 
 #pragma once
 
 /**
- * Simple text message printing implementation. This is a reference implementation for the demo
- * purposes only and used in the main program of `MDSANIMA INCREDIBLE` example demo poject.
+ * Simple text message printing implementation on the terminal stdout.
  *
- * @param text The text message to print in the terminal.
+ * @warning This is a reference implementation for the demo purposes only, and is used in the main
+ * example demo program for `MDSANIMA INCREDIBLE` project.
+ *
+ * @param text The text message to print on the terminal stdout.
  */
 void mprint(const char* text);
+
+/**
+ * Simple color text message printing implementation on the terminal stdout.
+ *
+ * @note You can change the color of the text message that will be printed on the terminal stdout.
+ * This function is a wrapper for the ANSI escape sequence.
+ *
+ * @warning This is a reference implementation for the demo purposes only, and is used in the main
+ * example demo program for `MDSANIMA INCREDIBLE` project.
+ *
+ * @param text The text message to print in the color on the terminal stdout.
+ * @param color The foreground text message color is a number from 0 to 255.
+ */
+void mprint(const char* text, int color);
--- example/mdsanima-incredible/main.cpp
+++ example/mdsanima-incredible/main.cpp
@@ -1,11 +1,13 @@
 // Copyright (c) 2024 MDSANIMA LAB. All rights reserved.
 // Licensed under the MIT license.
 
-// The main C++ program of `MDSANIMA INCREDIBLE` example demo project.
+// The main C++ example domo program for `MDSANIMA INCREDIBLE` project.
 
 #include "lib.hpp"
 
 auto main() -> int {
     const char* message = "Hello World from the MDSANIMA INCREDIBLE demo program!";
     mprint(message);
+    const int color = 2;
+    mprint(message, color);
 }
