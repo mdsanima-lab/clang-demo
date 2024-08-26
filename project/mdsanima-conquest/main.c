c304f778dfa4a46270ca157d1c3e884a8a3700e9
Marcin Różewski <marcinrozewski@gmail.com>
2024-08-27 00:30:14 +0200

    chore: update colors and formatting in all projects (#64)

--- project/mdsanima-conquest/main.c
+++ project/mdsanima-conquest/main.c
@@ -8,10 +8,10 @@
 int main(void)
 {
     const char *message = " MDSANIMA CONQUEST ";
-    const int sky       = 45;  // Text color
-    const int blue      = 27;  // Background color
+    const int   fgColor = 74;  // Text color
+    const int   bgColor = 62;  // Background color
 
-    cprint(message, sky, blue);
+    cprint(message, fgColor, bgColor);
 
     return 0;
 }
